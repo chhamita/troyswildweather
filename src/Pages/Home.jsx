e9a868ea30ca14cb42f739e377500190cb087764
chhamita <chh.amita.mca@gmail.com>
2023-09-19 17:49:00 +0530

    images change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import sun from 'img/sun.svg';
+import sun from '../../dist/img/sun.svg';
 
 function Home() {
     return (
