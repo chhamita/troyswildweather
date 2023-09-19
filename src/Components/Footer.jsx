b37e804717851e53d4acca32239aafb8ee6cc418
chhamita <chh.amita.mca@gmail.com>
2023-09-19 16:56:49 +0530

    latest code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
 function Footer() {
     return (
-
         <>
             <div className='bg-dark p-4'>
                 <p className='text-white text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu enim sodales, mattis elit eu, placerat nunc. Sed ultricies fermentum est eget bibendum.</p> 
