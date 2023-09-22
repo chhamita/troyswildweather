d9778960e19c33d53304ad8bc8d1e5b2c0dcd179
chhamita <chh.amita.mca@gmail.com>
2023-09-22 12:04:15 +0530

    Ip change

--- src/Utils/AxiosInstance.jsx
+++ src/Utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://api.weatherapi.com/v1', // Use HTTPS for secure connections
+  baseURL: 'https://api.weatherapi.com/v1', // Use HTTPS for secure connections
   headers: {
     "Content-Type": "application/json",
     "key": "87a7f6cf7ac6474b8fb134942231309", // Rename "key" to "apikey"
